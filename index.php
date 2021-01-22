9cf23d1a80dd7f493bc7319bf103ffaf0e2aa9b0
Daniele <daniele.bedeschi94@gmail.com>
2021-01-22 15:58:24 +0100

    Commit css

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/app.css">
+    <title>eSound</title>
+  </head>
+  <body>
+    <header>
+       <img src="https://pbs.twimg.com/profile_images/1274694068254265344/aQ0ltExT.jpg" alt="e-sound">
+       <h1>eSound</h1>
+    </header>
+  </body>
+</html>
