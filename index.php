cac9e415ce0fc8b324955015660c75d48b31bd0c
Daniele <daniele.bedeschi94@gmail.com>
2021-01-22 16:34:07 +0100

    Commit img stampate

--- index.php
+++ index.php
@@ -15,5 +15,21 @@
        <img src="https://pbs.twimg.com/profile_images/1274694068254265344/aQ0ltExT.jpg" alt="e-sound">
        <h1>eSound</h1>
     </header>
+    <main>
+      <?php
+        include __DIR__ .'/partials/db.php';
+        // var_dump($cd);
+      ?>
+      <div class="container-cd">
+        <div class="cd">
+          <?php foreach ($cd as $value) {?>
+            <img src="<?php echo $value["poster"]?>" alt="img">
+            <h3><?php echo $value["author"]?></h3>
+            <span><?php echo $value["title"]?></span>
+            <span><?php echo $value["year"]?></span>
+        <?php } ?>
+        </div>
+      </div>
+    </main>
   </body>
 </html>
